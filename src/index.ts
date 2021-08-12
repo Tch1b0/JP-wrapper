19ef5d00777f434d27033e6c3868b0e44b3fe1f7
tch1b0 <jpour3006@gmail.com>
2021-08-12 21:22:21 +0200

    remove tests

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,3 @@
-import { Post } from "./post";
-import { PostCollection } from "./postcollection";
-import { Requester } from "./requester";
-
 export { Post } from "./post";
 export { PostCollection } from "./postcollection";
 export { Requester } from "./requester";
-
-async function test() {
-	let r = new Requester();
-
-	let pc = new PostCollection(r);
-	let all_posts: Post[] = await pc.all();
-	let post = all_posts[0];
-
-	console.log(post);
-	console.log(await pc.all_titles());
-	console.log(await pc.count());
-
-	return null;
-}
-
-test();
